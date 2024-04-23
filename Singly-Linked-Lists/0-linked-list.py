9c59a39cf8fa1cae36f5c378ddbd30eeeb54394d
Tim Ngeno <timngeno424@gmail.com>
2024-04-23 15:29:36 +0300

    Merge two linked lists and sort them

--- Singly-Linked-Lists/0-linked-list.py
+++ Singly-Linked-Lists/0-linked-list.py
@@ -161,6 +161,44 @@ class LinkedList:
             return _reverse_recursive(curr, prev)
         self.head = _reverse_recursive(curr=self.head, prev=None)
 
+    def merge_sorted(self, llist):
+        """ Merges two linked lists and returns a sorted linked list """
+        p = self.head
+        q = llist.head
+        s = None
+
+        if not p:
+            return q
+        if not q:
+            return p
+
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+
+        if not p:
+            s.next = q
+
+        if not q:
+            s.next = p
+
+        self.head = new_head
+        return self.head
+
 
 llist = LinkedList()
 llist.append("A")
@@ -196,3 +234,24 @@ llist.delete_node_at_pos(0)
 
 llist.print_list()
 print(f'The length of the linked list: {llist.find_len()}')
+
+
+# Test sorting
+
+llist_1 = LinkedList()
+llist_2 = LinkedList()
+
+llist_1.append(1)
+llist_1.append(5)
+llist_1.append(7)
+llist_1.append(9)
+llist_1.append(10)
+
+llist_2.append(2)
+llist_2.append(3)
+llist_2.append(4)
+llist_2.append(6)
+llist_2.append(8)
+
+llist_1.merge_sorted(llist_2)
+llist_1.print_list()
