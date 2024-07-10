6cc8ef27d86d4302ae578182a8f2e74606d99231
Tim Ngeno <timngeno424@gmail.com>
2024-07-10 12:05:43 +0300

    Fast and slow pointers implementation in JS

---
+++ FastAndSlowPointers/0-pointer.js
@@ -0,0 +1,16 @@
+function hasCycle (head) {
+  if (!head || !head.next) { return false; }
+
+  let slow = head;
+  let fast = head.next;
+
+  while (slow !== fast) {
+    if (!fast || !fast.next) { return false; }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return true;
+}
+
+const node1 = new ListNode(1);
